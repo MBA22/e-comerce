d806305e0181e08db565c768f5d5f861f55177cd
Mario Barragan <mario.barragan.2012@gmail.com>
2014-06-24 23:22:07 -0500

    Add changes for conection

--- Connections/conexioncreacion.php
+++ Connections/conexioncreacion.php
@@ -3,7 +3,7 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_conexioncreacion = "localhost";
-$database_conexioncreacion = "creacionbd";
+$database_conexioncreacion = "e-clothes";
 $username_conexioncreacion = "root";
 $password_conexioncreacion = "";
 $conexioncreacion = mysql_pconnect($hostname_conexioncreacion, $username_conexioncreacion, $password_conexioncreacion) or trigger_error(mysql_error(),E_USER_ERROR); 
